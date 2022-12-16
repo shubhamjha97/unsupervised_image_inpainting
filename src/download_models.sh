3be1886ce3de5c3f4708160599f38fd2617a9d9f
sumanyumuku98 <mukusumanyu@gmail.com>
2022-12-16 15:50:53 -0800

    Moving from gshell to gdown

--- src/download_models.sh
+++ src/download_models.sh
@@ -1,5 +1,4 @@
 # weights link: https://drive.google.com/file/d/1gS6gMSqcXlN_hDQJT5dXT0QOYp6AjvNZ/view?usp=sharing
 DOWNLOAD_DIR = "./checkpoint"
-gshell init # gshell required to download files from google drive - https://pypi.org/project/gshell/
-gshell download --with-id 1gS6gMSqcXlN_hDQJT5dXT0QOYp6AjvNZ
+gdown download --id 1gS6gMSqcXlN_hDQJT5dXT0QOYp6AjvNZ
 unzip track1_models.zip -d ${DOWNLOAD_DIR}
